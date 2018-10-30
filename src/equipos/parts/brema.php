455894964999be5a1546c45c8d87c706c8587426
Renzo Aguilar <renzo.aguilar.rojas@gmail.com>
2018-10-30 00:59:11 -0500

    0004

---
+++ src/equipos/parts/brema.php
@@ -0,0 +1,76 @@
+<div class="row">
+    <div class="col-md-5 col-xs-12">
+        <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel" data-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item text-center active">
+                    <img class="d-block w-65" src="../images/lacimbali_lavatazas_bc35_01.jpg" alt="">
+                </div>
+                <div class="carousel-item text-center">
+                    <img class="d-block w-65" src="../images/lacimbali_lavatazas_bc35_02.jpg" alt="">
+                </div>
+                <div class="carousel-item text-center">
+                    <img class="d-block w-65" src="../images/lacimbali_lavatazas_bc35_03.jpg" alt="">
+                </div>
+               
+            </div>
+            <ol class="carousel-indicators carousel-indicators2">
+                <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active">
+                    <img class="d-block w-100" src="../images/lacimbali_lavatazas_bc35_01.jpg" alt="">
+                </li>
+                <li data-target="#carouselExampleIndicators1" data-slide-to="1">
+                    <img class="d-block w-100" src="../images/lacimbali_lavatazas_bc35_02.jpg" alt="">
+                </li>
+                <li data-target="#carouselExampleIndicators1" data-slide-to="2">
+                    <img class="d-block w-100" src="../images/lacimbali_lavatazas_bc35_03.jpg" alt="">
+                </li>
+            </ol>
+        </div>
+    </div>
+    <div class="col-md-7 col-xs-12 equipos-detalle">
+        <div class="text-center">
+            <h3>LAVATAZAS BC35</h3>
+        </div>
+        <p>El lavaplatos BC35 de La Cimbali cuenta con una carrocería de acero inoxidable doble pared, salpicadero electromecánico, temporizador electrónico, teclas retroiluminadas, circuito de lavado y aclarado de acero inoxidable por separado, circuito de lavado inferior y giratorio, circuito de aclarado inferior y superior giratorio, doble filtro de aspiración, dosificador de abrillantador peristáltico de serie con peso y filtro de inserción y por ultimo función de ablandador de agua.</p>
+        <img class="tabla" src="../images/lacambali001.jpg" alt="">
+    </div>
+</div>
+
+<div class="row margin-top-40">
+    <div class="col-md-5 col-xs-12">
+        <div id="carouselExampleIndicators3" class="carousel slide" data-ride="carousel" data-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item text-center active">
+                    <img class="d-block w-65" src="../images/lacimbali_lavatazas_bc40_01.jpg" alt="">
+                </div>
+                <div class="carousel-item text-center">
+                    <img class="d-block w-65" src="../images/lacimbali_lavatazas_bc40_02.jpg" alt="">
+                </div>
+                <div class="carousel-item text-center">
+                    <img class="d-block w-65" src="../imageslacimbali_lavatazas_bc40_03.jpg" alt="">
+                </div>
+            
+            </div>
+            <ol class="carousel-indicators carousel-indicators2">
+                <li data-target="#carouselExampleIndicators3" data-slide-to="0" class="active">
+                    <img class="d-block w-100" src="../images/lacimbali_lavatazas_bc40_01.jpg" alt="">
+                </li>
+                <li data-target="#carouselExampleIndicators3" data-slide-to="1">
+                    <img class="d-block w-100" src="../images/lacimbali_lavatazas_bc40_02.jpg" alt="">
+                </li>
+                <li data-target="#carouselExampleIndicators3" data-slide-to="2">
+                    <img class="d-block w-100" src="../images/lacimbali_lavatazas_bc40_03.jpg" alt="">
+                </li>
+               
+            </ol>
+        </div>
+    </div>
+    <div class="col-md-7 col-xs-12 equipos-detalle">
+        <div class="text-center">
+            <h3>LAVATAZAS BC40</h3>
+        </div>
+       <p>El lavaplatos BC40 de La Cimbali cuenta con una carrocería de acero inoxidable doble pared, salpicadero electromecánico, temporizador electrónico, teclas retroiluminadas, circuito de lavado y aclarado de acero inoxidable por separado, circuito de lavado inferior y giratorio, circuito de aclarado inferior y superior giratorio, doble filtro de aspiración, dosificador de abrillantador peristáltico de serie con peso y filtro de inserción y por ultimo función de ablandador de agua.
+    </p>
+        <img class="tabla" src="../images/lacambali002.jpg" alt="">
+    </div>
+</div>
+
